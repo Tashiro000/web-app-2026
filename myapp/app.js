969a6d97bb84772a22e13530d06e58015c1fa422
Tasiro000 <nk250123.nks.iput@example.com>
2026-06-08 21:46:55 +0900

    po-tobangouhenkou

--- myapp/app.js
+++ myapp/app.js
@@ -74,6 +74,6 @@ app.post('/api/messages', async (req, res) => {
   res.json(result.rows[0]);
 }); 
 
-app.listen(3000, () => {
-  console.log('サーバーが起動しました: http://localhost:3000');
+app.listen(3036, () => {
+  console.log('サーバーが起動しました: http://localhost:3036');
 });
